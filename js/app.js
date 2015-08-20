79ad1e2b323d42a4f8a647438c9a4ba7362a1d63
petertrung <petertrung@comcast.net>
2015-08-20 13:33:55 -0700

    Ajax

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
-$(document).ready(function(){
 
+$(document).ready(function(){
 
   var catData = [
     {
@@ -12,113 +12,121 @@ $(document).ready(function(){
       value: 1,
       color: '#0000ff'
     }
-  ]
+  ];
 
   var catOptions = {
     segmentShowStroke : false,
-    percentageInnerCutout: 75,
+    percentageInnerCutout: 50,
     animateScale : true
-  }
-//Chart
+  };
+
+
   var cats = document.getElementById('catChart').getContext('2d');
   var catChart = new Chart(cats).Pie(catData, catOptions);
- //Constructor Function
-   var Photo = function(fileLocation){//  constructor
+
+  var Photo = function(fileLocation){
     this.path = fileLocation;
     this.vote = 1;
-   }
+  }
 
-   //new objects 
-   var pic1 = new Photo('img/1.jpg');
-   var pic2 = new Photo('img/2.jpg');
-   var pic3 = new Photo('img/3.jpg');
-   var pic4 = new Photo('img/4.jpg');
-   var pic5 = new Photo('img/5.jpg');
-   var pic6 = new Photo('img/6.jpg');
-   var pic7 = new Photo('img/7.jpg');
-   var pic8 = new Photo('img/8.jpg');
-   var pic9 = new Photo('img/9.jpg');
-   var pic10 = new Photo('img/10.jpg');
-   var pic11 = new Photo('img/11.jpg');
-   var pic12 = new Photo('img/12.jpg');
-   var pic13 = new Photo('img/13.jpg');
-   var pic14 = new Photo('img/14.jpg');
+  //new objects
+  var pic1 = new Photo('img/1.jpg');
+  var pic2 = new Photo('img/2.jpg');
+  var pic3 = new Photo('img/3.jpg');
+  var pic4 = new Photo('img/4.jpg');
+  var pic5 = new Photo('img/5.jpg');
+  var pic6 = new Photo('img/6.jpg');
+  var pic7 = new Photo('img/7.jpg');
+  var pic8 = new Photo('img/8.jpg');
+  var pic9 = new Photo('img/9.jpg');
+  var pic10 = new Photo('img/10.jpg');
+  var pic11 = new Photo('img/11.jpg');
+  var pic12 = new Photo('img/12.jpg');
+  var pic13 = new Photo('img/13.jpg');
+  var pic14 = new Photo('img/14.jpg');
 
-   var photoArray =[pic1,pic2,pic3,pic4,pic5,pic6,pic7,pic8,pic9,pic10,pic11,pic12,pic13,pic14];
+  var photoArray =[pic1,pic2,pic3,pic4,pic5,pic6,pic7,pic8,pic9,pic10,pic11,pic12,pic13,pic14];
 
-   var rightPhoto = document.getElementById('rightPhoto');
+  var rightPhoto = $('#rightPhoto');
 
-   var leftPhoto = document.getElementById('leftPhoto');
+  var leftPhoto = $('#leftPhoto');
 
-   var reset = document.getElementById('reset');
+  var pics = [];
+$.ajax({
+  url: 'https://api.imgur.com/3/album/DDoWy.json',
+  method: 'GET',
+  header: {
+    'Authorization':'Client-ID 1c8b975d1fc9b45',
+  }
+}).done(function(res) {
+  pics= res.data.images;
+  console.log(pics)
+  for (var i = 0; i < pics.length; i++) {
+    photoArray[i].path= pics[i].link;
+  };
+  
+})
 
-//Connects all the functions
-var cat = function(){
-  //hides reset button
-$('#reset').hide();
-//Generates Random Number to choose which number in the array
   var randomPic = function() {
     return Math.floor(Math.random()*(14 - 0));
-
   };
-//Chooses Photo for left and right from the array
-   var Tracker = function(){
+
+  var Tracker = function(){
     this.leftPhoto = photoArray[randomPic()];
     this.rightPhoto = photoArray[randomPic()];
     console.log(this.leftPhoto);
     console.log(this.rightPhoto);
-//Makes it find a new photo if the right and the left images are the same
-    while (this.leftPhoto === this.rightPhoto) {
+
+    while (this.leftPhoto.path === this.rightPhoto.path) {
       this.rightPhoto = photoArray[randomPic()];
     }
-   }
-//define the value of both sides of the graph
-   var tracker = new Tracker();
-   catData[0].value = tracker.leftPhoto.vote;
-   catData[1].value = tracker.rightPhoto.vote;
-//Function registered if right pic is clicked
+  };
+
+  var tracker = new Tracker();
+  catData[0].value = tracker.leftPhoto.vote;
+  catData[1].value = tracker.rightPhoto.vote;
+
   Tracker.prototype.rightVote = function(){
-    catChart.segments[0].value++
+    tracker.rightPhoto.vote++
+    //console.log(tracker.rightPhoto.vote);
+    catChart.segments[0].value = tracker.rightPhoto.vote;
     catChart.update();
-    //show reset
-    $('#reset').show();
+  };
 
-  }
-//Function registered if left pic is clicked
-   Tracker.prototype.leftVote = function(){
-    catChart.segments[1].value++
+  Tracker.prototype.leftVote = function(){
+    tracker.leftPhoto.vote++
+    //console.log(tracker.leftPhoto.vote);
+    catChart.segments[1].value = tracker.leftPhoto.vote;
+    $('#leftPhoto').style.background.color = "blue";
     catChart.update();
-    //show reset
-    $('#reset').show();
+  };
 
-  }
-//Resets the score of the graph and picks a new image
-    Tracker.prototype.reset = function(){
-  catChart.segments[0].value = 1;
-  catChart.segments[1].value = 1;
-  catChart.update();
-  //hide reset
-  cat();
-}
+  Tracker.prototype.reset = function(){
+    tracker.leftPhoto = photoArray[randomPic()];
+    tracker.rightPhoto = photoArray[randomPic()];
+    catChart.segments[0].value = tracker.rightPhoto.vote;
+    catChart.segments[1].value = tracker.leftPhoto.vote;
+    tracker.displayPhotos();
+    catChart.update();
 
+    if (tracker.leftPhoto.path === tracker.rightPhoto.path) {
+      tracker.reset();
+    }
 
-//displays the Photos
+  };
+
   Tracker.prototype.displayPhotos = function() {
     var rightContent = '<img src="' + this.rightPhoto.path + '" />';
     var leftContent = '<img src="' + this.leftPhoto.path + '" />';
-    rightPhoto.innerHTML = rightContent;
-    leftPhoto.innerHTML = leftContent;
+    rightPhoto.html(rightContent);
+    leftPhoto.html(leftContent);
   }
-rightPhoto.addEventListener('click',tracker.rightVote);
-leftPhoto.addEventListener('click',tracker.leftVote);
-reset.addEventListener('click',tracker.reset);
-  
 
-tracker.displayPhotos();
-}
+  $('#rightPhoto').click(tracker.rightVote);
+  $('#leftPhoto').click(tracker.leftVote);
+  $('#reset').click(tracker.reset);
 
 
-
-cat();
+  tracker.displayPhotos();
 
 });
